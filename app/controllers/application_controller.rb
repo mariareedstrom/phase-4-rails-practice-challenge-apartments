442f19a11eb32c089aa14940776a0a0705995aa9
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-08-30 12:22:05 -0500

    Commit at 57 min.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :respond_when_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :respond_when_record_invalid
+
+  private
+
+  def respond_when_not_found(error)
+    render json: {error: error.message}, status: :not_found
+  end
+
+  def respond_when_record_invalid
+    render json: {errors: ["validation errors" ]}, status: :unprocessable_entity
+  end
+
 end
+
